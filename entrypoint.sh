81e29be809a8b377ee7aa59029e0c551c9f7ab31
tomohiko.sugiyama <tomohiko.sugiyama@sprocket.bz>
2023-12-19 16:06:16 +0900

    feat: change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+echo "$PWD"
+
+sh -c "git config --global --add safe.directory $PWD"
+
+git config --get credential.helper
+
 # git remote のリストを取得し、各リモートについての URL を表示する
 for remote in $(git remote); do
     echo "Remote name: $remote"
